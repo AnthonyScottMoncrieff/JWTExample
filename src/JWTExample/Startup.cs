54ad57ccb64fc561b3e2a863b02a745dc32cb89e
AnthonyScottMoncrieff <anthony.scottmoncrieff@gmail.com>
2020-08-13 17:54:14 +0100

    Fixed in memory db issue

--- src/JWTExample/Startup.cs
+++ src/JWTExample/Startup.cs
@@ -3,11 +3,15 @@ using JWTExample.Data;
 using JWTExample.Domain.Middleware;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using System;
+using System.IO;
+using System.Reflection;
 
 namespace JWTEample
 {
@@ -23,10 +27,11 @@ namespace JWTEample
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase(databaseName: "Accounts"));
             IOC.RegisterDependencies(services, Configuration);
-            services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()));
             services.AddControllers();
             services.AddMvc();
+            RegisterSwagger(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,10 +48,43 @@ namespace JWTEample
 
             app.UseMiddleware<JWTMiddleware>();
 
+            ConfigureSwagger(app);
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private static void RegisterSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo { Title = "JWTExample API Documentation", Version = "v1" });
+                c.IncludeXmlComments(GetXmlCommentsPath());
+            });
+        }
+
+        private static void ConfigureSwagger(IApplicationBuilder app)
+        {
+            // Rewrite ecommercegateway swagger to root
+            app.UseRewriter(new RewriteOptions()
+                .AddRewrite(@"^JWTExample/swagger(.*)$", "swagger$1", true));
+
+            app.UseStaticFiles();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("swagger/v1/swagger.json", "User API V1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+
+        private static string GetXmlCommentsPath()
+        {
+            return Path.Combine(AppContext.BaseDirectory, $"{Assembly.GetExecutingAssembly().GetName().Name}.xml");
         }
     }
 }
